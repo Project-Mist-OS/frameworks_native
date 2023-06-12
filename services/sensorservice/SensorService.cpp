b4928b2810295039525a06b07e64032d960c6aaf
Vladimir Komsiyski <vladokom@google.com>
2023-06-12 10:23:53 +0200

    Handle runtime sensor events even if there are no real ones.
    SensorService::threadLoop() "continues" if there are no real sensor
    events, which skips the handling of runtime sensor events.
    
    Fix: 281452823
    Test: m
    Change-Id: I0fe9fe9a7baa19ce9c8cb32d36e736ac83d2fe52
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- services/sensorservice/SensorService.cpp
+++ services/sensorservice/SensorService.cpp
@@ -1138,9 +1138,14 @@ bool SensorService::threadLoop() {
     do {
         ssize_t count = device.poll(mSensorEventBuffer, numEventMax);
         if (count < 0) {
-            if (count == DEAD_OBJECT && device.isReconnecting()) {
-                handleDeviceReconnection(device);
-                continue;
+            if(count == DEAD_OBJECT && device.isReconnecting()) {
+                device.reconnect();
+                // There are no "real" events at this point, but do not skip the rest of the loop
+                // if there are pending runtime events.
+                Mutex::Autolock _l(&mLock);
+                if (mRuntimeSensorEventQueue.empty()) {
+                    continue;
+                }
             } else {
                 ALOGE("sensor poll failed (%s)", strerror(-count));
                 break;
